bbe8c077c7e1c6c7f3b24251e51fff494ccddcd4
igor <souzaamorimdasilvai@gmail.com>
2024-02-16 11:26:07 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://dashboard.render.com/web/srv-cn3nfcnqd2ns73eild2g/deploys/dep-cn7mnja1hbls73dsis9g'
+    baseURL: 'https://rocketnotes-w8zn.onrender.com'
 });
