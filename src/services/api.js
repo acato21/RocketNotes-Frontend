1a54c26470671c11a2d29aa357c846409cdd486c
igor <souzaamorimdasilvai@gmail.com>
2024-02-16 11:22:54 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://dashboard.render.com/web/srv-cn3nfcnqd2ns73eild2g/deploys/dep-cn7mnja1hbls73dsis9g'
 });
